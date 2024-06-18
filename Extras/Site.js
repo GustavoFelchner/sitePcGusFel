518c9cc6e993b3dcea2356988f6131fe79c51669
GustavoFelchner <gustavo.felchner@escola.pr.gov.br>
2024-06-18 15:34:59 -0300

    Delete Extras directory

--- Extras/Site.js
+++
@@ -1,5 +0,0 @@
-var n=1;
-function nextImg(){if(n==1){document.getElementById('s1').style.backgroundImage='url(https://www.intensivacursos.com.br/wp-content/uploads/curso-conserto-manutencao-celulares-1.png.webp)'; n=2} else {document.getElementById('s1').style.backgroundImage='url(https://semifree.com.br/wp-content/uploads/2020/10/manutencao_de_celulares_e_smartphones_598076516.jpg)'; n=1}};
-function aumenta(){document.getElementById('next').style.height='45px'; document.getElementById('next').style.width='45px'};
-function diminui(){document.getElementById('next').style.height='40px'; document.getElementById('next').style.width='40px'};
-function fechar(){document.getElementById('popUp1').style.display='none';}
